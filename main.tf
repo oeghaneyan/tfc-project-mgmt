e3c6d269367e8bb6a2cc454b63b500c2f22e6967
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-04 13:39:05 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "tfe" {
   hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
   token    = var.token
-  version  = "~> 0.42.0"
 }
 
 resource "tfe_project" "project" {
