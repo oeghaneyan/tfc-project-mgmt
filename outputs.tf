4b5603b095b3625a6aafe686a371ece793f1400b
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-04 15:04:02 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-#output "project_id" {
-#  description = "IDs of each project created"
-#  value       = tfe_project.project.[each.key].id
-#}
+output "project_id" {
+  description = "IDs of each project created"
+  value       = tfe_project.project.*.id
+}
