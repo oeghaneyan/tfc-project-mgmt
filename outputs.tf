586cab827856434bbacdb465cba33fef65de1d7f
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 15:34:57 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "project_list" {
   description = "List of projects including names, IDs, and organization of project"
-  value       = { for p in sort(keys(var.project_list)) : p => module.tfe_project[p].id }
-  #  value       =  tfe_project.project[*]
+  #value       = { for p in sort(keys(var.project_list)) : p => module.tfe_project[p].id }
+  value       =  tfe_project.project[*]
 }
