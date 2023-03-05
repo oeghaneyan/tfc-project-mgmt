0d3362afd66b7caa7682ecffce8efb365362e60b
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 15:43:58 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "project_list" {
   description = "List of projects including names, IDs, and organization of project"
-  value       = [ for project in tfe_project.project : project.name ]
+  value       = [ for project in tfe_project.project : project.id ]
   #value       =  tfe_project.project[*]
 }
