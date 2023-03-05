d2f74e611757d2aad383d3205d80a89a5bec1a59
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 07:00:01 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,8 @@ provider "tfe" {
   token    = var.token
 }
 
-#resource "tfe_project" "project" {
-#  count = length(var.project_name)
-#  organization = var.org_name
-#  name  = var.project_name[count.index]
-#}
-
 resource "tfe_project" "project" {
   organization = var.org_name
   for_each = toset(var.project_list)
   name     = each.key
-}
-
-resource "tfe_project" "project-test" {
-  organization = var.org_name
-  name     = "project-test"
 }
