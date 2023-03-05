9dc80ef9c93068e7bc8e87acf83fa8ac4d90b7f9
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 15:29:27 -0800

    Update main.tf

--- modules/tfe_project/main.tf
+++ modules/tfe_project/main.tf
@@ -1,8 +1,3 @@
-provider "tfe" {
-  hostname = var.hostname # Optional, defaults to Terraform Cloud `app.terraform.io`
-  token    = var.token
-}
-
 resource "tfe_project" "project" {
   organization = var.org_name
   for_each = toset(var.project_list)
