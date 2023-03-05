aa70a2d62279116358a004d89ef41b2cdcd92bb6
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 06:42:31 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,10 @@
+#output "project_id" {
+#  description = "IDs of each project created"
+#  for_each = toset(var.project_list)
+#  value       = tfe_project.project.*.*.id
+#}
+
 output "project_id" {
   description = "IDs of each project created"
-  for_each = toset(var.project_list)
-  value       = tfe_project.project.*.*.id
+  value       = tfe_project.project-test.id
 }
