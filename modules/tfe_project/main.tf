bcdc249cf3e85aaa715822f92eb325f6aef57d78
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 22:56:26 -0800

    Delete modules directory

--- modules/tfe_project/main.tf
+++
@@ -1,5 +0,0 @@
-resource "tfe_project" "project" {
-  organization = var.org_name
-  for_each = toset(var.project_list)
-  name     = each.key
-}
--- modules/tfe_project/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "project_list" {
-  description = "List of projects including names, IDs, and organization of project"
-  value       =  tfe_project.project[*]
-}
