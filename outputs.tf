a681b94abedbb7a3864a9f4a0ee3c77f8e09f566
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-03-05 16:17:15 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,7 @@ output "project_list" {
   description = "List of projects including names, IDs, and organization of project"
   value       = {
     for name, project in tfe_project.project : name => project.id])
+  }
 #  value       = toset([ for project in tfe_project.project : project.id])
 #  value       = [ for project in tfe_project.project : project.id ]
 #  value       =  tfe_project.project[*]
